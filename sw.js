f3b24122dbe8a6b157d4c9e62e608865080344c3
Letícia Cerial <leticia.cerial@aluno.senai.br>
2026-04-28 11:09:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'sorvete-front';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './images/logo.png',
+    './images/logo64.png',
+    './images/logo512.png',
+    './images/banner.png',
+    './images/whatsapp.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Fazendo whey no cache!');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
